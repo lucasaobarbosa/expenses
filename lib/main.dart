590da46b6c38064a1358c01a634057b67c366187
lucasaobarbosa <lucasaobarbosa@gmai.com>
2021-09-10 20:35:21 -0300

    1.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 
+import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(ExpensesApp());
@@ -12,15 +13,48 @@ class ExpensesApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+    class MyHomePage extends StatelessWidget {
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo Tênis de Corrida',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+        Transaction(
+      id: 't2',
+      title: 'Conta de Luz',
+      value: 211.20,
+      date: DateTime.now(),
+    ),
+  ];
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Despesas Pessoais'),
       ),
-      body: Center(
-        child: Text('Versão Inicial'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            child: Card(
+              color: Colors.blue,
+              child: Text('Grafico'),
+              elevation: 5,
+            ),
+          ),
+          Column(
+            children: _transactions.map((tr) {
+            return Card(
+              child: Text(tr.title),
+            );
+          }).toList(),
+         ),
+        ],
       ),
     );
   }
